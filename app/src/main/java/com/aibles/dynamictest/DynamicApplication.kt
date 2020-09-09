f4a7fd799b354ba3396ec9ca3ca72d0ae4988951
Nguyen Quoc Khanh <nguyenkhanhcv@gmail.com>
2020-09-09 14:56:33 +0700

    WIP: add new feature

---
+++ app/src/main/java/com/aibles/dynamictest/DynamicApplication.kt
@@ -0,0 +1,14 @@
+package com.aibles.dynamictest
+
+import android.app.Application
+import android.content.Context
+import com.google.android.play.core.splitcompat.SplitCompat
+
+class DynamicApplication: Application() {
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        SplitCompat.install(this)
+    }
+
+}
--- app/src/main/java/com/aibles/dynamictest/MainActivity.kt
+++ app/src/main/java/com/aibles/dynamictest/MainActivity.kt
@@ -2,10 +2,18 @@ package com.aibles.dynamictest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.play.core.splitinstall.SplitInstallManager
+import com.google.android.play.core.splitinstall.SplitInstallManagerFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private val manager: SplitInstallManager by lazy {
+        SplitInstallManagerFactory.create(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
